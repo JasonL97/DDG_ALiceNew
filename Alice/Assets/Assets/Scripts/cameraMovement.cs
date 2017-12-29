75262bbcd5d42e5c281f70c7aa251c4333ac54a7
ShaneLimJiXuan <33240428+ShaneLimJiXuan@users.noreply.github.com>
2017-12-29 12:43:29 +0800

    Camera Fixed
    Camera now no longer move away from character but does not rotate.
    
    Idea: Have a trigger such that when the player move pass the trigger the camera rotate to let player have a better view of current room

---
+++ Alice/Assets/Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour {
+    public GameObject player;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y+12, player.transform.position.z); // move camera according to player position
+    }
+}
